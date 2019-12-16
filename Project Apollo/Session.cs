e5833c3e03d56b58b7a0728d5098e73df307729e
Zontreck <tarapiccari@gmail.com>
2019-12-16 03:39:39 -0700

    Initial commit

---
+++ Project Apollo/Session.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Project_Apollo.Registry;
+using System.Text;
+
+namespace Project_Apollo
+{
+    public sealed class Session
+    {
+        private static readonly object _lock = new object();
+        private static Session _inst;
+
+        static Session() { }
+        public static Session Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if(_inst == null)
+                    {
+                        _inst = new Session();
+                    }
+                    return _inst;
+                }
+            }
+        }
+
+        // Begin vars!
+        public HttpListener ProductionListen;
+        public APIRegistry Registry;
+        // End vars!
+    }
+}

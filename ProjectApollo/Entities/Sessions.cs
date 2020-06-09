21b88056ac996a405c03a6e47c8ad6a41183191f
Robert Adams <misterblue@misterblue.com>
2020-06-09 09:50:04 -0700

    Sessions: mark heartbeat updated session as updated so DB is written.

--- ProjectApollo/Entities/Sessions.cs
+++ ProjectApollo/Entities/Sessions.cs
@@ -148,6 +148,7 @@ namespace Project_Apollo.Entities
             if (TryGetSessionWithSenderKey(pSenderKey, out ret))
             {
                 ret.TimeOfLastHeartbeat = DateTime.UtcNow;
+                ret.Updated();
             }
             else
             {

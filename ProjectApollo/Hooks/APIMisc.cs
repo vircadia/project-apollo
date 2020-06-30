4f942f1b12b686b0b001c9edd2b1f10df50618b8
Robert Adams <misterblue@misterblue.com>
2020-06-30 07:53:12 -0700

    Generate metaverse_info response with each call.     This allows runtime updating of MetaverseInfo.json

--- ProjectApollo/Hooks/APIMisc.cs
+++ ProjectApollo/Hooks/APIMisc.cs
@@ -1,4 +1,4 @@
-//   Copyright 2020 Vircadia
+﻿//   Copyright 2020 Vircadia
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -30,49 +30,43 @@ namespace Project_Apollo.Hooks
         public static readonly string _logHeader = "[APIMisc]";
 
         // == GET /api/metaverse_info ==================================
-        private static string _metaverse_info;
-
         [APIPath("/api/metaverse_info", "GET", true)]
         public RESTReplyData get_metaverse_info(RESTRequestData pReq, List<string> pArgs)
         {
             RESTReplyData replyData = new RESTReplyData();  // The HTTP response info
 
-            if (_metaverse_info == null)
+            JObject jo = new JObject
             {
-                JObject jo = new JObject
+
+                // Start with the basic parameters
+                ["metaverse_name"] = Context.Params.P<string>(AppParams.P_METAVERSE_NAME),
+                ["metaverse_nick_name"] = Context.Params.P<string>(AppParams.P_METAVERSE_NICKNAME),
+                ["metaverse_url"] = Context.Params.P<string>(AppParams.P_METAVERSE_SERVER_URL),
+                ["ice_server_url"] = Context.Params.P<string>(AppParams.P_DEFAULT_ICE_SERVER),
+                ["metaverse_server_version"] = ThisAssembly.AssemblyInformationalVersion
+            };
+
+            // See if there are additions in the info file
+            string infoFile = EntityStorage.GenerateAbsStorageLocation(null, Context.Params.P<string>(AppParams.P_METAVERSE_INFO_FILE));
+            if (File.Exists(infoFile))
+            {
+                try
                 {
-
-                    // Start with the basic parameters
-                    ["metaverse_name"] = Context.Params.P<string>(AppParams.P_METAVERSE_NAME),
-                    ["metaverse_nick_name"] = Context.Params.P<string>(AppParams.P_METAVERSE_NICKNAME),
-                    ["metaverse_url"] = Context.Params.P<string>(AppParams.P_METAVERSE_SERVER_URL),
-                    ["ice_server_url"] = Context.Params.P<string>(AppParams.P_DEFAULT_ICE_SERVER),
-                    ["metaverse_server_version"] = ThisAssembly.AssemblyInformationalVersion
-                };
-
-                // See if there are additions in the info file
-                string infoFile = EntityStorage.GenerateAbsStorageLocation(null, Context.Params.P<string>(AppParams.P_METAVERSE_INFO_FILE));
-                if (File.Exists(infoFile))
+                    string fContents = File.ReadAllText(infoFile);
+                    JObject jContents = JObject.Parse(fContents);
+                    foreach (JProperty jprop in jContents.Properties())
+                    {
+                        jo[jprop.Name] = jprop.Value;
+                    }
+                }
+                catch (Exception e)
                 {
-                    try
-                    {
-                        string fContents = File.ReadAllText(infoFile);
-                        JObject jContents = JObject.Parse(fContents);
-                        foreach (JProperty jprop in jContents.Properties())
-                        {
-                            jo[jprop.Name] = jprop.Value;
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Context.Log.Error("{0} Exception reading metaverse info file {1}: {2}",
-                                            _logHeader, infoFile, e);
-                    }
-                }   
-                _metaverse_info = JsonConvert.SerializeObject(jo, Formatting.Indented);
-            }
+                    Context.Log.Error("{0} Exception reading metaverse info file {1}: {2}",
+                                        _logHeader, infoFile, e);
+                }
+            }   
 
-            replyData.Body = _metaverse_info;
+            replyData.Body = JsonConvert.SerializeObject(jo, Formatting.Indented);
 
             return replyData;
         }

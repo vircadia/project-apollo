f0ae75ad2934991256b6501eb97e6f60cd65a090
Robert Adams <misterblue@misterblue.com>
2020-06-30 07:13:24 -0700

    Fix line endings to be all UNIX form.

--- ProjectApollo/Entities/Users.cs
+++ ProjectApollo/Entities/Users.cs
@@ -1,4 +1,4 @@
-﻿//   Copyright 2020 Vircadia
+//   Copyright 2020 Vircadia
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
